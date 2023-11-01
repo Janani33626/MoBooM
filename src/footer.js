ab4a642511a0414abd7b615e22e892d4b3f13d1f
Janani33626 <jananiravishankar412@gmail.com>
2023-11-01 07:05:38 +0530

    first commit

---
+++ src/footer.js
@@ -0,0 +1,43 @@
+import React from "react"
+import "./footer.css"
+
+
+function Footer(){
+    return(
+        <div class="footerContainer">
+            <div class="footerList1">
+                
+                <ul class="footerListItems">
+                    <li><h5>CompanyInfo</h5></li>
+                    <li>About</li>
+                    <li>Social responsibility</li>
+                    <li>Affilate</li>
+                    <li>Blogger</li>
+                </ul>
+            </div>
+            <div class="footerList2">
+                
+                <ul class="footerListItems">
+                    <li><h5>HELP and SUPPORT</h5></li>
+                    <li>Shipping Info</li>
+                    <li>How to order</li>
+                    <li>How to Track</li>
+                    <li>Size chrt</li>
+                </ul>
+            </div>
+            <div class="footerList3">
+                
+                <ul class="footerListItems">
+                    <li><h5>CompanyInfo</h5></li>
+                    <li>About</li>
+                    <li>Social responsibility</li>
+                    <li>Affilate</li>
+                    <li>Blogger</li>
+                </ul>
+            </div>
+        </div>
+
+    )
+}
+
+export default Footer
---
+++ src/header.js
@@ -0,0 +1,18 @@
+import React from "react"
+import {Container, Row, Col} from "react-bootstrap";
+function Header(){
+    return(
+        <Container>
+                <Row>
+                    <Col className="center-align">
+                        <h1> MoBooM</h1>
+                    </Col>
+                </Row>
+            </Container>
+    )
+    
+    
+
+}
+
+export default Header 
